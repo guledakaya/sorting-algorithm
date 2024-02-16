e36a8c3ea64e7485e6430f743c3b50b584f981f8
guledakaya <gul.edaa.kaya@gmail.com>
2024-02-17 00:01:53 +0300

    Add project files.

---
+++ sorting algorithm/sorting algorithm.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+void insertionsort(int* A, int n);
+
+int main()
+{
+	int A[6] = { 5, 2, 4, 6, 1, 3 };
+	
+	int n = sizeof(A) / sizeof(int);
+
+	insertionsort(A, n);
+
+	for (int i = 0; i < n; i++) {
+		printf("%d,", A[i]);
+	}
+
+	getchar();
+}
+void insertionsort(int* A, int n) {
+	
+	int key = 0;
+	int i = 0;
+
+	for (int j = 1; j < n; j++) {
+		
+		key = A[j];
+		i = j - 1;
+		while (i > -1 && A[i] > key) {
+			A[i + 1] = A[i];
+			i = i - 1;
+		}
+		A[i + 1] = key;
+	}
+}
+
